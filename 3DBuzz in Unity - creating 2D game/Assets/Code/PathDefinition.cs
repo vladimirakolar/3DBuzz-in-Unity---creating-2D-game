4de0bd6da71a88c2cf860d39315438ca59996f84
Vladimira <vladimirakolar@gmail.com>
2019-12-27 14:21:53 +0100

    create the lop in class.

--- 3DBuzz in Unity - creating 2D game/Assets/Code/PathDefinition.cs
+++ 3DBuzz in Unity - creating 2D game/Assets/Code/PathDefinition.cs
@@ -7,9 +7,24 @@ public class PathDefinition : MonoBehaviour
 {
     public Transform[] Points;
 
-    public IEnumerator<Transform> GetPathsEnumerator()
+    public IEnumerator<Transform> GetPathEnumerator()
     {
-        throw new NotImplementedException();
+        if (Points == null || Points.Length < 1)
+            yield break;
+
+        var direction = 1;
+        var index = 0;
+        while (true)
+        {
+            yield return Points[index];
+
+            if (index <= 0)
+                direction = 1;
+            else if (index >= Points.Length - 1)
+                direction = -1;
+
+            index = index + direction;
+        }
     }
 
     public void OnDrawGizmos()
