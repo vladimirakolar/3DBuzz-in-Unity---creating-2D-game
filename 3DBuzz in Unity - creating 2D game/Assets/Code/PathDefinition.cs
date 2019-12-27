97b7f0c00db1782877e08698fcd1d656b84f51b0
Vladimira <vladimirakolar@gmail.com>
2019-12-27 13:58:48 +0100

    Create new empetz object and set to the one perent.

--- 3DBuzz in Unity - creating 2D game/Assets/Code/PathDefinition.cs
+++ 3DBuzz in Unity - creating 2D game/Assets/Code/PathDefinition.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class PathDefinition : MonoBehaviour
 {
     public Transform[] Points;
 
+    public IEnumerator<Transform> GetPathsEnumerator()
+    {
+        throw new NotImplementedException();
+    }
 
+    public void OnDrawGizmos()
+    {
+        if (Points.Length < 2)
+            return;
+
+        for (var i = 1; i < Points.Length; i++)
+        {
+            Gizmos.DrawLine(Points[i - 1].position, Points[i].position);
+        }
+    }
 }
