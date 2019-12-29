74dcb228a3db39052ae21a9dbb67f4b96abd1db0
Vladimira <vladimirakolar@gmail.com>
2019-12-29 19:23:31 +0100

    Create new script for ControllerParametars2D.

---
+++ 3DBuzz in Unity - creating 2D game/Assets/Code/ControllersParameters2D.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+
+public class ControllersParameters2D
+{
+    public enum JumpBehavior
+    {
+        CanJumpGround,
+        CanJumpAnywhere,
+        CanJump
+    }
+
+    public Vector2 MaxVelocity = new Vector2(float.MaxValue, float.MaxValue);
+
+    [Range(0, 90)]
+    public float SlopeLimit = 30;
+
+    public float Gravity = -25f;
+
+    public JumpBehavior JumpRestrictions;
+
+    public float JumpFrequency = .25f;
+
+}
