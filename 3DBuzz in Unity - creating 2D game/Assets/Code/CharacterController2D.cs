8b6fbec3620d116ede247e5b8e0eaa4187188153
Vladimira <vladimirakolar@gmail.com>
2019-12-30 15:13:52 +0100

    Create the move method in CharacterController.

--- 3DBuzz in Unity - creating 2D game/Assets/Code/CharacterController2D.cs
+++ 3DBuzz in Unity - creating 2D game/Assets/Code/CharacterController2D.cs
@@ -14,34 +14,56 @@ public class CharacterController2D : MonoBehaviour
     public LayerMask PlatforMask;
     public ControllersParameters2D DefaultParameters;
 
-    public ControllersParameters2D State { get; private set; }
-    public Vector2 Velocity { get; private set;}
+    public ControllerState2D State { get; private set; }
+    public Vector2 Velocity { get { return _velocity} }
     public bool CantJump { get { return false; } }
+    public bool HandleCollisions { get; set; }
+    public ControllersParameters2D Parameters { get { return _overrideParameters ?? DefaultParameters; } }
+
+    private Vector2 _velocity;
+    private Transform _transform;
+    private Vector3 _localScale;
+    private BoxCollider2D _boxCollider;
+    private ControllersParameters2D _overrideParameters;
+
+    private float 
+        _verticalDistanceBetweenRays,
+        _horizontalDistanceBetweenRays;
 
     public void Awake()
     {
-        State = new ControllersParameters2D();
+      // State = new ControllersParameters2D();
+        State = new ControllerState2D();
+        _transform = transform;
+        _localScale = transform.localScale;
+        _boxCollider = GetComponent<BoxCollider2D>();
+
+        var colliderWidth = _boxCollider.size.x * Mathf.Abs(transform.localScale.x) - (2 * SkinWidth); 
+        _horizontalDistanceBetweenRays = colliderWidth / (TotalVerticalRays - 1);
+
+        var colliderHeight = _boxCollider.size.y * Mathf.Abs(transform.localScale.y) - (2 * SkinWidth);
+        _verticalDistanceBetweenRays = colliderHeight / (TotalHorizontalRays - 1);
     }
 
    
     public void AddForce(Vector2 force)
     {
-
+        _velocity = force;
     }
   
     public void SetForce(Vector2 force)
     {
-
+        _velocity += force;
     }
    
     public void SetHorizontalForce(float x)
     {
-
+        _velocity.x = x;
     }
   
     public void SetVerticalForce(float y)
     {
-
+        _velocity.y = y;
     }
    
     public void Jump()
@@ -51,11 +73,39 @@ public class CharacterController2D : MonoBehaviour
   
     public void LateUpdate()
     {
-        
+        Move(Velocity * Time.deltaTime);
     }
     private void Move(Vector2 deltaMovement)
- 
     {
+        var wasGrounded = State.IsCollidingBelow;
+        State.Reset();
+
+        if (HandleCollisions)
+        {
+            HandlePlatforms();
+            CalculateRayOrigins();
+
+            if (deltaMovement.y < 0 && wasGrounded)
+                HandeleVerticalSlope(ref deltaMovement);
+
+            if (Mathf.Abs(deltaMovement.x) > .001f)
+                MoveHorizontally(ref deltaMovement);
+
+            MoveVertically(ref deltaMovement);
+        }
+
+        _transform.Translate(deltaMovement, Space.World);
+
+        //ToDo: moving platform code.
+
+        if (Time.deltaTime > 0)
+            _velocity = deltaMovement / Time.deltaTime;
+
+        _velocity.x = Mathf.Min(_velocity.x, Parameters.MaxVelocity.x);
+        _velocity.y = Mathf.Min(_velocity.y, Parameters.MaxVelocity.y);
+
+        if (State.IsMovingUpSlope)
+            _velocity = 0;
         
     }
 
