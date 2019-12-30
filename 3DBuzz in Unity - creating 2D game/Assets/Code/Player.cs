6fe98cf8fc11cb950d5f057b16f75e1dce249250
Vladimira <vladimirakolar@gmail.com>
2019-12-30 12:30:41 +0100

    Create new code for player.

--- 3DBuzz in Unity - creating 2D game/Assets/Code/Player.cs
+++ 3DBuzz in Unity - creating 2D game/Assets/Code/Player.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool _isFacingRight;
+    private CharacterController2D _controller;
+    private float _normalizedHorizontalSpeed;
+
+    public float MaxSpeed = 8;
+    public float SpeedAccelerationGround = 10f;
+    public float SpeedAccelerationInAir = 5f;
+
+    public void Start()
     {
-        
+        _controller = GetComponent<CharacterController2D>();
+        _isFacingRight = transform.localScale.x > 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        HandleInput();
+
+        var MovementFactor = _controller.State.IsGrounded ? SpeedAccelerationGround : SpeedAccelerationInAir;
+        _controller.SetHorizontalForce(Mathf.Lerp(_controller.Velocity.x, _normalizedHorizontalSpeed * MaxSpeed, Time.deltaTime * MovementFactor));
     }
+
+    private void HandleInput()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            _normalizedHorizontalSpeed = 1;
+            if (!_isFacingRight)
+                Flip();
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            _normalizedHorizontalSpeed = -1;
+            if (_isFacingRight)
+                Flip();
+        }
+        else
+        {
+            _isFacingRight = 0;
+        }
+        if (_controller.CanJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            _controller.Jump();
+        }
+    }
+
+    private void Flip()
+    {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        _isFacingRight = transform.localScale.x > 0;
+    }
+
 }
