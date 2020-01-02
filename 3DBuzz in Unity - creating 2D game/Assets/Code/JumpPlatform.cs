cd57a0d9bd44d2117ade9300b86d5a82cb232442
Vladimira <vladimirakolar@gmail.com>
2020-01-02 19:15:19 +0100

    Wrotted a new script.

--- 3DBuzz in Unity - creating 2D game/Assets/Code/JumpPlatform.cs
+++ 3DBuzz in Unity - creating 2D game/Assets/Code/JumpPlatform.cs
@@ -1,18 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float JumpMagnitude = 20;
 
-    // Update is called once per frame
-    void Update()
+    public void ControllerEnter2D(CharacterController2D controller)
     {
-        
+        controller.SetVerticalForce(JumpMagnitude);
     }
 }
