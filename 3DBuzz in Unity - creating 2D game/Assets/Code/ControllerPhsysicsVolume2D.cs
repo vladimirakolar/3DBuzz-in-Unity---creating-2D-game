278f076101a4ef7e4c154804304a21940418a9b5
Vladimira <vladimirakolar@gmail.com>
2020-01-03 13:32:10 +0100

    Wroted code to a new created script ControllerPhsysicsVolume2D.

--- 3DBuzz in Unity - creating 2D game/Assets/Code/ControllerPhsysicsVolume2D.cs
+++ 3DBuzz in Unity - creating 2D game/Assets/Code/ControllerPhsysicsVolume2D.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class ControllerPhsysicsVolume2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public ControllersParameters2D Parameters;
+    
 }
