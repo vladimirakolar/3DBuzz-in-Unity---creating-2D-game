3943ce22f7c541f362d7835e824102c819a52755
Vladimira <vladimirakolar@gmail.com>
2020-01-03 15:29:39 +0100

    Writed new code in script BackgroundParallax.

--- 3DBuzz in Unity - creating 2D game/Assets/Code/BackgroundParallax.cs
+++ 3DBuzz in Unity - creating 2D game/Assets/Code/BackgroundParallax.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class BackgroundParallax : MonoBehaviour
 {
+    public Transform[] Background;
+    public float ParallaxScale;
+    public float ParallexReductionFactor;
+    public float Smoothing;
 
+    private Vector3 _lastPosition;
+
+    public void Start()
+    {
+        _lastPosition = transform.position;
+    }
+
+    public void Update()
+    {
+        var parallax = (_lastPosition.x - transform.position.x) * ParallaxScale;
+
+        for (var i = 0; i < Background.Length; i++)
+        {
+            var bacgroundTargetPosition = Background[i].position.x + parallax * (i * ParallexReductionFactor + 1);
+            Background[i].position = Vector3.Lerp(
+                Background[i].position, 
+                new Vector3(bacgroundTargetPosition, Background[i].position.y, Background[i].position.z),
+                Smoothing * Time.deltaTime);
+        }
+
+        _lastPosition = transform.position;
+    }
 }
