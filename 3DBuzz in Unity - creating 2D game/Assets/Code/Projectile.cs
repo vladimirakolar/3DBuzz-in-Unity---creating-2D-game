213e3d9665742407b32220094bef97d6d9e6e8f5
Vladimira <vladimirakolar@gmail.com>
2020-01-06 21:27:42 +0100

    Sets up the projectile system that can be shared between our player and other actors in the game.

---
+++ 3DBuzz in Unity - creating 2D game/Assets/Code/Projectile.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Projectile : MonoBehaviour
+{
+    public float Speed;
+    public LayerMask CollisionMask;
+
+    public GameObject Owner { get; private set; }
+    public Vector2 Direction { get; private set; }
+    public Vector2 InitialVelocity { get; private set; }
+
+    public void Initialized(GameObject owner, Vector2 direction, Vector2 initialVelocity)
+    {
+        Owner = owner;
+        Direction = direction;
+        InitialVelocity = initialVelocity;
+        OnInitialized();
+    }
+
+    protected virtual void OnInitialized()
+    {
+
+    }
+
+    public virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((CollisionMask.value & (1 << other.gameObject.layer))==0)
+        {
+            OnNotCollideWith(other);
+            return;
+        }
+
+        var isOwner = other.gameObject == Owner;
+        if (isOwner)
+        {
+            OnCollideOwner();
+            return;
+        }
+
+        var takeDamage = (ITakeDamage)other.GetComponent(typeof(ITakeDamage));
+        if (takeDamage != null)
+        {
+            OnCollideTakeDamage(other, takeDamage);
+            return;
+        }
+
+        OnCollideOther(other);
+    }
+
+    protected virtual void OnNotCollideWith(Collider2D other)
+    {
+
+    }
+
+    protected virtual void OnCollideOwner()
+    {
+
+    }
+
+    protected virtual void OnCollideTakeDamage (Collider2D other, ITakeDamage takeDamage)
+    {
+
+    }
+
+    protected virtual void OnCollideOther (Collider2D other)
+    {
+
+    }
+}
---
+++ 3DBuzz in Unity - creating 2D game/Assets/Code/SimpleProjectile.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleProjectile : Projectile , ITakeDamage
+{
+    public int Damage;
+    public GameObject DestroyedEffect;
+    public int PointsToGiveToPlayer;
+    public float TimeToLive;
+
+    public void Update()
+    {
+        if ((TimeToLive -= Time.deltaTime)<= 0)
+        {
+            DestroyProjectile();
+            return;
+        }
+
+        transform.Translate((Direction + new Vector2(InitialVelocity.x, 0)) * Speed * Time.deltaTime, Space.World);
+    }
+
+    public void TakeDamage(int damage, GameObject instigator)
+    {
+        if (PointsToGiveToPlayer != 0)
+        {
+            var projectile = instigator.GetComponent<Projectile>();
+            if (projectile != null && projectile.Owner.GetComponent<Player>() != null)
+            {
+                GameManager.Instance.AddPoints(PointsToGiveToPlayer);
+                FloatingText.Show(string.Format("+{0}!", PointsToGiveToPlayer), "PointStarText", new FromWorldPointTextPositioner(Camera.main, transform.position, 1.5f, 50));
+            }
+        }
+
+        DestroyProjectile();
+    }
+
+    protected override void OnCollideOther(Collider2D other)
+    {
+        DestroyProjectile();
+    }
+
+    protected override void OnCollideTakeDamage(Collider2D other, ITakeDamage takeDamage)
+    {
+        takeDamage.TakeDamage(Damage, gameObject);
+        DestroyProjectile();
+    }
+
+    private void DestroyProjectile()
+    {
+        if (DestroyedEffect != null)
+            Instantiate(DestroyedEffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+}
